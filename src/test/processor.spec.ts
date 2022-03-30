7105748047dec31cf68ef959830e2b49ea56459d
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2022-03-30 13:48:26 +0100

    refactor: change let to const for immutability

--- src/test/processor.spec.ts
+++ src/test/processor.spec.ts
@@ -110,7 +110,7 @@ describe("Processor", () => {
 
 describe("Instructions", () => {
     it(`RET: 0x${Instructions.RET.toString(16)}, restores the PC from the stack`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x2204, 0x0000, 0x00EE]);
         processor.decode_instruction(processor.fetch_instruction());
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x204);
@@ -118,13 +118,13 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x202);
     });
     it(`GOTO: 0x${Instructions.GOTO.toString(16)}, moves the PC to the desired location`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x1016]);
         processor.decode_instruction(processor.fetch_instruction());
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x16);
     });
     it(`CALL: 0x${Instructions.CALL.toString(16)}, saves the PC to stack before moving it to the desired location`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x2204, 0x0000, 0x00EE]);
         processor.decode_instruction(processor.fetch_instruction());
         expect(processor.system_memory[0x0]).is.equal(0x2);
@@ -133,21 +133,21 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x204);
     });
     it(`SKPIM: 0x${Instructions.SKPIM.toString(16)}, skip the next instruction if the specified register is equal to immediate value`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x3103]);
         processor.registers[RegisterNames.V1].value = 3;
         processor.decode_instruction(processor.fetch_instruction());
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x204);
     });
     it(`SKPNIM: 0x${Instructions.SKPNIM.toString(16)}, skip the next instruction if the specified register is not equal to immediate value`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x4004]);
         processor.registers[RegisterNames.V0].value = 3;
         processor.decode_instruction(processor.fetch_instruction());
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x204);
     });
     it(`SKPEQ: 0x${Instructions.SKPEQ.toString(16)}, skip the next instruction if the specified register is equal to the other register`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x5010]);
         processor.registers[RegisterNames.V0].value = 3;
         processor.registers[RegisterNames.V1].value = 3;
@@ -155,19 +155,19 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x204);
     });
     it(`ASHEX: 0x${Instructions.ASHEX.toString(16)}, assign hex value to specified register`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.decode_instruction(0x60AA);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0xAA);
     });
     it(`ADD: 0x${Instructions.ADD.toString(16)}, add immediate value to the specified register`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xA;
         expect(processor.registers[RegisterNames.V0].value).is.equal(0xA);
         processor.decode_instruction(0x705A);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0x64);
     });
     it(`COPY: 0x${Instructions.COPY.toString(16)}, copy the value from one register to another`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xA;
         expect(processor.registers[RegisterNames.V0].value).is.equal(0xA);
         expect(processor.registers[RegisterNames.V1].value).is.equal(0x0);
@@ -175,28 +175,28 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.V1].value).is.equal(0xA);
     });
     it(`OR: 0x${Instructions.OR.toString(16)}, perform a logical OR operator against two registers`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xAB;
         processor.registers[RegisterNames.V1].value = 0xC;
         processor.decode_instruction(0x8011);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0xAF);
     });
     it(`AND: 0x${Instructions.AND.toString(16)}, perform a logical AND operator against two registers`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xAB;
         processor.registers[RegisterNames.V1].value = 0xC;
         processor.decode_instruction(0x8012);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0x8);
     });
     it(`XOR: 0x${Instructions.XOR.toString(16)}, perform a logical XOR operator against two registers`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xAB;
         processor.registers[RegisterNames.V1].value = 0xC;
         processor.decode_instruction(0x8013);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0xA7);
     });
     it(`ADDF: 0x${Instructions.ADDF.toString(16)}, add two register together and detect if overflow occurs`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xFF;
         processor.registers[RegisterNames.V1].value = 0xFF;
         processor.decode_instruction(0x8014);
@@ -209,7 +209,7 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.VF].value).is.equal(0x0);
     });
     it(`SUBY: 0x${Instructions.SUBY.toString(16)}, subtract two registers and detect if underflow occurs`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xFF;
         processor.registers[RegisterNames.V1].value = 0xFE;
         processor.decode_instruction(0x8015);
@@ -222,7 +222,7 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.VF].value).is.equal(0x0);
     });
     it(`SHR: 0x${Instructions.SHR.toString(16)}, right shift value detect if LSB was one`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V1].value = 0x010;
         processor.decode_instruction(0x8016);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0x8);
@@ -233,7 +233,7 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.VF].value).is.equal(0x1);
     });
     it(`SUBX: 0x${Instructions.SUBX.toString(16)}, subtract two registers and detect if underflow occurs`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0xFF;
         processor.registers[RegisterNames.V1].value = 0xFE;
         processor.decode_instruction(0x8017);
@@ -246,7 +246,7 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.VF].value).is.equal(0x1);
     });
     it(`SHL: 0x${Instructions.SHL.toString(16)}, left shift value detect if MSB was one`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V1].value = 0x010;
         processor.decode_instruction(0x801E);
         expect(processor.registers[RegisterNames.V0].value).is.equal(0x20);
@@ -257,7 +257,7 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.VF].value).is.equal(0x1);
     });
     it(`SKPNEQ: 0x${Instructions.SKPNEQ.toString(16)}, skip the next instruction if the specified register is not equal to the other register`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.load_program([0x9010]);
         processor.registers[RegisterNames.V0].value = 2;
         processor.registers[RegisterNames.V1].value = 3;
@@ -265,18 +265,18 @@ describe("Instructions", () => {
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x204);
     });
     it(`SIP: 0x${Instructions.SIP.toString(16)}, set the index pointer to the immediate value`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.decode_instruction(0xA010);
         expect(processor.registers[RegisterNames.IP].value).is.equal(0x10);
     });
     it(`GOTOV: 0x${Instructions.GOTOV.toString(16)}, jump to the sum of the immediate address and V0`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.V0].value = 0x10;
         processor.decode_instruction(0xB010);
         expect(processor.registers[RegisterNames.PC].value).is.equal(0x20);
     });
     it(`RBAND: 0x${Instructions.RBAND.toString(16)}, generate a random byte and perform a logical AND against it and the immediate value`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         for (let counter = 0; counter < 1_000; counter++) {
             processor.decode_instruction(0xC0AF);
             expect(processor.registers[RegisterNames.V0].value).is.below(0x100);
@@ -292,7 +292,7 @@ describe("Instructions", () => {
     it(`SPITCH: 0x${Instructions.SPITCH.toString(16)}, set the pitch of the tone generator to VX`);
     it(`STONE: 0x${Instructions.STONE.toString(16)}, set the sound tone timer for twenty times VX`);
     it(`ADDIP: 0x${Instructions.ADDIP.toString(16)}, add VX to the index pointer`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.IP].value = 0x10;
         processor.registers[RegisterNames.V0].value = 0x10;
         processor.decode_instruction(0xF01E);
@@ -301,7 +301,7 @@ describe("Instructions", () => {
     it(`DSPDIG: 0x${Instructions.DSPDIG.toString(16)}, set the index pointer to the digit found in VX`);
     it(`DSPCHR: 0x${Instructions.DSPCHR.toString(16)}, set the index pointer to the ASCII character found in VX`);
     it(`DEQ: 0x${Instructions.DEQ.toString(16)}, store a three digit decimal equivalent of VX in memory`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.IP].value = 0x100;
         processor.registers[RegisterNames.V0].value = 0x80;
         processor.decode_instruction(0xF033);
@@ -311,7 +311,7 @@ describe("Instructions", () => {
 
     });
     it(`STORE: 0x${Instructions.STORE.toString(16)}, store the contents of the registers from V0 to VX in memory`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.IP].value = 0x100;
         processor.registers[RegisterNames.V0].value = 0x80;
         processor.registers[RegisterNames.V1].value = 0x10;
@@ -322,7 +322,7 @@ describe("Instructions", () => {
         expect(processor.system_memory[0x102]).is.equal(0xAF);
     });
     it(`LOAD: 0x${Instructions.LOAD.toString(16)}, load into registers V0 to VX contents from memory`, () => {
-        let processor = new Processor(4096);
+        const processor = new Processor(4096);
         processor.registers[RegisterNames.IP].value = 0x100;
         processor.system_memory[0x100] = 0x10;
         processor.system_memory[0x101] = 0x20;
