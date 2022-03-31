dee30259d61261b258460d028d9fe1967ec9e709
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2022-03-31 17:56:24 +0100

    feat: add font data to system memory

--- src/test/memory.spec.ts
+++ src/test/memory.spec.ts
@@ -1,9 +1,15 @@
 import { expect } from "chai";
-import { create_memory } from "memory";
+import { create_memory, font_data } from "memory";
 
 describe("Memory", () => {
     it("Array constructed has reserved space for 4096 elements", () => {
         const system_memory: Uint8Array = create_memory(4096);
         expect(system_memory.length).is.equal(4096);
+    });
+    it("Memory initialised with font data at 0x50", () => {
+        const system_memory: Uint8Array = create_memory(4096);
+        for(let index = 0; index < font_data.length; index++) {
+            expect(system_memory[index + 0x50]).is.equal(font_data[index]);
+        }
     });
 });
